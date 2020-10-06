2701d4b65a1cdf21b8300a71693108a6615ca8f6
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-10-06 19:22:28 +1100

    Server listening on 5050

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+//app variables
+const app = express();
+const port = process.env.PORT || 5050;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//setting up mongodb connection
+const uri = process.env.ATLAS_URI; // paste the mongodb connection string as ATLAS_URI in .env file
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch(err => {
+    console.error(`Error connecting to DB ${err} !`);
+  });
+
+// homepage route
+app.get("/", (req, res) => {
+  res.send(`The MERN express API is live now!`);
+});
+
+//listening to the port
+app.listen(port, () => {
+  console.log(` MERN Express API listening on ${port} !`);
+});
